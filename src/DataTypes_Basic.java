6db8354534af9bbf83815335077776b8ce0f4103
manepullavamsi1998 <manepullavamsi1998@gmail.com>
2021-10-28 08:22:01 +0530

    JAva

---
+++ src/DataTypes_Basic.java
@@ -0,0 +1,7 @@
+public class DataTypes_Basic {
+    public static void main(String []args){
+
+    }
+}
+    git config --global user.email "you@example.com"
+        git config --global user.name "Your Name"
